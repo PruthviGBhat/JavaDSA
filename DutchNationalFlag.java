a876da85f8471df9b287b72fd7a7b0955d373242
Pruthvi Bhat <pruthvigbhaveri@gmail.com>
2024-08-23 18:03:49 +0530

    dutchnationalflag added

---
+++ DutchNationalFlag.java
@@ -0,0 +1,31 @@
+class DutchNationalFlag{
+    public static void sortColors(int[] nums) {
+        int left=0;
+        int looper=0;
+        int right=nums.length-1;
+        while(looper<=right){
+            if(nums[looper]==0){
+                swap(nums,looper,left);
+                left++;
+                looper++;
+            }else if(nums[looper]==2){
+                swap(nums,looper,right);
+                right--;
+            }else{
+                looper++;
+            }
+        }
+    }
+    public static void swap(int nums[],int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
